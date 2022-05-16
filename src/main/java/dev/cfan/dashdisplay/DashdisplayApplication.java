2f25a608ef4e540f900b5f2f6215594d9f23f34f
Christopher Fanning <chris@christopherfanning@gmail.com>
2022-05-16 07:31:35 -0500

    Add spring starter

---
+++ src/main/java/dev/cfan/dashdisplay/DashdisplayApplication.java
@@ -0,0 +1,13 @@
+package dev.cfan.dashdisplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashdisplayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashdisplayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/cfan/dashdisplay/DashdisplayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cfan.dashdisplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashdisplayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
